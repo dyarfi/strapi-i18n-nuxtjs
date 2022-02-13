f0df6c0a8541a62c0bd2bb8df5848c587622a183
Defrian Yarfi <defrian.yarfi@gmail.com>
2022-02-13 15:32:40 +0700

    chore: update onenction string

--- config/database.js
+++ config/database.js
@@ -1,25 +1,21 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  console.log(host, port, database, user, password);
-  return {
-    defaultConnection: "default",
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        // host: env("DATABASE_HOST", "127.0.0.1"),
-        // port: env.int("DATABASE_PORT", 5432),
-        // database: env("DATABASE_NAME", "strapi-postgres"),
-        // user: env("DATABASE_USERNAME", "postgres"),
-        // password: env("DATABASE_PASSWORD", "example"),
-        host,
-        port,
-        database,
-        username: user,
-        password,
-        ssl: env.bool("DATABASE_SSL", false),
-      },
+      // postgres://bxgnmxyi:sJ_qFpCBGIg_qt88juXHGMP7jQUTb6jk@john.db.elephantsql.com/bxgnmxyi
+      host: env("DATABASE_HOST", "john.db.elephantsql.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "bxgnmxyi"),
+      user: env("DATABASE_USERNAME", "bxgnmxyi"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      password: env("DATABASE_PASSWORD", "sJ_qFpCBGIg_qt88juXHGMP7jQUTb6jk"),
+      // host: env("DATABASE_HOST", "127.0.0.1"),
+      // port: env.int("DATABASE_PORT", 5432),
+      // database: env("DATABASE_NAME", "strapi-postgres"),
+      // user: env("DATABASE_USERNAME", "postgres"),
+      // schema: env("DATABASE_SCHEMA", "public"),
+      // password: env("DATABASE_PASSWORD", "example"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
-  };
-};
+  },
+});
