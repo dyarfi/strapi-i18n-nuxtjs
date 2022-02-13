12c3fea0206a561b97d508d77317e7494d33e1f6
Defrian Yarfi <defrian.yarfi@gmail.com>
2022-02-13 14:42:40 +0700

    chore: update pg connection string

--- config/database.js
+++ config/database.js
@@ -1,12 +1,21 @@
+const { parse } = require("pg-connection-string");
+
 module.exports = ({ env }) => ({
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi-postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'example'),
+      // host: env('DATABASE_HOST', '127.0.0.1'),
+      // port: env.int('DATABASE_PORT', 5432),
+      // database: env('DATABASE_NAME', 'strapi-postgres'),
+      // user: env('DATABASE_USERNAME', 'postgres'),
+      // password: env('DATABASE_PASSWORD', 'example'),
+      host,
+      port,
+      database,
+      username: user,
+      password,
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
